6163798bad37f51566b6fe1670b2bf3e0b05e83f
Killaot <klaot.ledantec@gmail.com>
2023-10-03 17:00:54 +0200

    first commit

---
+++ assets/component/exemple.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>{{ greetingMessage }}</h1>
+    
+    </template>
+    
+    
+    <script>
+    export default {
+        name: 'exemple',
+        props: {
+            greetingMessage: {
+                type: String,
+                default: 'gg la team'
+            }
+        }
+    };
+    </script>
+    
+    
+    <style>
+    h1 {
+        color: blue;
+    }
+    </style>
+    
+    
