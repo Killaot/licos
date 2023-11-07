821f0f5d7274280b019302b9f3ba245ff9bcf670
RoseLeCoz <rlecoz.ledantec@gmail.com>
2023-11-07 17:20:36 +0100

    test

--- src/Entity/Colis.php
+++ src/Entity/Colis.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Metadata\ApiResource;
 
+
 #[ORM\Entity(repositoryClass: ColisRepository::class)]
 #[ApiResource]
 class Colis
