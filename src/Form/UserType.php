d1ad6139b006fc6a3dd5dd0450b0c031b0eeb5f3
Killaot <klaot.ledantec@gmail.com>
2023-12-11 08:49:28 +0100

    thanos

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Entity\Ville;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class UserType extends AbstractType
 {
@@ -13,14 +16,17 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('isVerified')
+            
             ->add('nom')
             ->add('prenom')
             ->add('telephone')
             ->add('adresse')
-            ->add('idVille')
+            ->add('idVille', EntityType::class, [
+                'class' => Ville::class,
+                'choice_label' => 'Nom', // ou tout autre champ à afficher dans le menu déroulant
+                'placeholder' => 'Choose a city', // texte optionnel à afficher en haut du menu déroulant
+                // ...
+            ])
         ;
     }
 
