d77380e06fc02d3f17e94b267af60a161d798159
Majaj13 <merrermael1309@gmail.com>
2023-12-12 16:18:18 +0100

    wow

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
+
 class MapController extends AbstractController
 {
     #[Route('/map', name: 'app_map')]
